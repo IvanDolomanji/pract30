90854e66c5b6c0650750f7fa9d3af1dcedb6348a
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-03-27 10:57:56 +0200

    Add files via upload

---
+++ ConsoleApp8/Magazine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Magazine : PrintedProducts
+    {
+        private int edition;
+        private int price;
+
+        public int Edition { get => edition; set => edition = value; }
+        public int Price { get => price; set => price = value; }
+
+        public Magazine() { }
+        public Magazine(string name, int edition, int price) : base(name)
+        {
+            this.Edition = edition;
+            this.Price = price;
+            this.Name = name;
+        }
+        public override double TotalCost()
+        {
+            return Price * Edition;
+        }
+        public override void Info()
+        {
+            Console.WriteLine($"Журнал: {Name}\nЦена: {Price}\nТираж: {Edition}");
+        }
+         
+        public static Magazine Enter()
+        {
+            Console.Clear();
+            Console.Write("Введите название журнала: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите тираж журнала: ");
+            int edition = int.Parse(Console.ReadLine());
+            Console.Write("Введите цену журнала: ");
+            int price = int.Parse(Console.ReadLine());
+            return new Magazine
+                (name, edition, price);
+
+
+        }
+    }
+}
